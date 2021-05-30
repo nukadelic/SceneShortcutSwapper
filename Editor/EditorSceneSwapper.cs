63048868b768c209a7f349f6c78d4257fbb3e13c
Nukadelic <vladik.voina@gmail.com>
2021-05-31 01:52:58 +0700

    Update EditorSceneSwapper.cs

--- Editor/EditorSceneSwapper.cs
+++ Editor/EditorSceneSwapper.cs
@@ -1,11 +1,11 @@
+#if UNITY_EDITOR
 
 using UnityEngine;
-using UnityEditor;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEditor.SceneManagement;
 
-namespace editor
+namespace UnityEditor
 {
     public class EditorSceneSwapper : EditorWindow
     {
@@ -13,13 +13,13 @@ namespace editor
         static void HookShortcuts()
         {
             AssemblyReloadEvents.beforeAssemblyReload += Unhook;
-            HookGlobalEvent( true );
+            HookGlobalEvent(true);
         }
 
         static void Unhook()
         {
             AssemblyReloadEvents.beforeAssemblyReload -= Unhook;
-            HookGlobalEvent( false );
+            HookGlobalEvent(false);
         }
         static void HookGlobalEvent(bool add)
         {
@@ -36,23 +36,23 @@ namespace editor
         {
             var e = Event.current;
 
-            if( e == null || ! e.isKey ) return;
-            if (e.type != EventType.KeyDown ) return;
-            if( ! e.alt ) return;
-            
-            var code = ( int ) e.keyCode;
-            if( code < 48 || code > 57 ) return;
+            if (e == null || !e.isKey) return;
+            if (e.type != EventType.KeyDown) return;
+            if (!e.alt) return;
+
+            var code = (int)e.keyCode;
+            if (code < 48 || code > 57) return;
 
             var key_index = code == 48 ? 0 : code - 49;
             var settings = EditorSceneSwapperSettings.instance;
-            if ( key_index > settings.m_Items.Count - 1 ) return;
+            if (key_index > settings.m_Items.Count - 1) return;
 
-            EditorSceneManager.OpenScene( settings.m_Items[ key_index ].GetPath() );
+            EditorSceneManager.OpenScene(settings.m_Items[key_index].GetPath());
         }
 
-
-        [MenuItem("Tools/Scene Swapper")] // Use `Tools / Quick Actions / Scene Swapper` instead  ? 
-        public static void ShowWindow() => EditorWindow.GetWindow< EditorSceneSwapper>( "Scene Swapper" ).minSize = new Vector2( 400, 200 );
+        [MenuItem("Tools/Scene Swapper")]
+        public static void ShowWindow() => GetWindow<EditorSceneSwapper>
+            ("Tools / Scene Swapper").minSize = new Vector2(400, 200);
 
         string[] GetBuildScenes()
         {
@@ -85,62 +85,62 @@ namespace editor
         {
             bool save = false;
 
-            if( scenes == null ) scenes = GetBuildScenes();
+            if (scenes == null) scenes = GetBuildScenes();
 
             var settings = EditorSceneSwapperSettings.instance;
 
-            GUILayout.Space( 10 );
+            GUILayout.Space(10);
 
-            EditorGUILayout.HelpBox( "Assign shortcut to open a scene build index or scene asset", MessageType.Info );
+            EditorGUILayout.HelpBox("Assign shortcut to open a scene build index or scene asset", MessageType.Info);
 
             GUILayout.Space(10);
 
-            if ( settings.m_Items == null || settings.m_Items.Count > 0 ) 
+            if (settings.m_Items != null && settings.m_Items.Count > 0)
             {
-                using( new GUILayout.ScrollViewScope( scroll ) )
+                using (new GUILayout.ScrollViewScope(scroll))
                 {
                     int index = 0; int remove = -1;
 
-                    foreach( var item in settings.m_Items )
+                    foreach (var item in settings.m_Items)
                     {
                         EditorGUI.BeginChangeCheck();
 
                         using (new GUILayout.HorizontalScope())
                         {
-                            GUILayout.Space( 10 );
+                            GUILayout.Space(10);
 
-                            GUILayout.Label( "Alt+" + ( index + 1 ) , GUILayout.Width( 55 ) );
+                            GUILayout.Label("Alt+" + (index + 1), GUILayout.Width(55));
 
-                            item.useBuildIndex = GUILayout.Toggle( item.useBuildIndex, item.useBuildIndex ? "Build Index" : "Scene" );
+                            item.useBuildIndex = GUILayout.Toggle(item.useBuildIndex, item.useBuildIndex ? "Build Index" : "Scene");
 
                             GUILayout.Space(10);
 
-                            if ( item.useBuildIndex )
+                            if (item.useBuildIndex)
                             {
-                                if( item.buildIndex > scenes.Length - 1 ) item.buildIndex = scenes.Length - 1;
+                                if (item.buildIndex > scenes.Length - 1) item.buildIndex = scenes.Length - 1;
 
-                                item.buildIndex = EditorGUILayout.IntSlider( item.buildIndex, 0, scenes.Length - 1 );
+                                item.buildIndex = EditorGUILayout.IntSlider(item.buildIndex, 0, scenes.Length - 1);
                             }
                             else
                             {
-                                var value = EditorGUILayout.ObjectField( item.sceneAssetReference, typeof(SceneAsset) , true );
+                                var value = EditorGUILayout.ObjectField(item.sceneAssetReference, typeof(SceneAsset), true);
 
-                                item.sceneAssetReference = (SceneAsset) value;
+                                item.sceneAssetReference = (SceneAsset)value;
                             }
 
                             GUILayout.Space(5);
 
-                            if( GUILayout.Button("[ x ]", GUILayout.Width( 35 ) ) )
+                            if (GUILayout.Button("[ x ]", GUILayout.Width(35)))
                             {
                                 remove = index;
                             }
 
-                            GUILayout.Space( 10 );
+                            GUILayout.Space(10);
                         }
 
-                        ++ index;
+                        ++index;
 
-                        if( EditorGUI.EndChangeCheck() ) save = true;
+                        if (EditorGUI.EndChangeCheck()) save = true;
                     }
 
                     if (remove > -1)
@@ -151,27 +151,30 @@ namespace editor
                     }
                 }
             }
-            else GUILayout.Label("No Items, click + to add");
+            else 
+            {
+                EditorGUILayout.HelpBox("No items, Press [ + ] to create", MessageType.Info);
 
-            using( new GUILayout.HorizontalScope() )
+                GUILayout.FlexibleSpace();
+            }
+            using (new GUILayout.HorizontalScope())
             {
                 GUILayout.FlexibleSpace();
 
-                if( GUILayout.Button( "[ + ]", GUILayout.Width(35) ) )
+                if (GUILayout.Button("[ + ]", GUILayout.Width(35)))
                 {
-                    if( settings.m_Items == null ) settings.m_Items = new List<EditorSceneSwapperSettings.Item>();
+                    if (settings.m_Items == null) settings.m_Items = new List<EditorSceneSwapperSettings.Item>();
 
-                    settings.m_Items.Add( new EditorSceneSwapperSettings.Item { useBuildIndex = true, buildIndex = settings.m_Items.Count } );
-
+                    settings.m_Items.Add(new EditorSceneSwapperSettings.Item { useBuildIndex = true, buildIndex = settings.m_Items.Count });
                     save = true;
                 }
-                
+
                 GUILayout.Space(10);
             }
 
-            GUILayout.Space( 10 );
+            GUILayout.Space(10);
 
-            if( save ) settings.Save();
+            if (save) settings.Save();
         }
     }
 
@@ -187,9 +190,9 @@ namespace editor
 
             public string GetPath()
             {
-                if( useBuildIndex ) return SceneUtility.GetScenePathByBuildIndex( buildIndex );
+                if (useBuildIndex) return SceneUtility.GetScenePathByBuildIndex(buildIndex);
 
-                return AssetDatabase.GetAssetPath( sceneAssetReference );
+                return AssetDatabase.GetAssetPath(sceneAssetReference);
             }
         }
 
@@ -199,3 +202,5 @@ namespace editor
         public void Save() => Save(true);
     }
 }
+
+#endif
